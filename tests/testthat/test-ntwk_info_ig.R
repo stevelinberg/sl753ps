fd52a51a38cf8bc9b5534439c848d97e722660e5
Steve Linberg <steve@slinberg.net>
2022-04-20 19:57:13 -0400

    Add sna graph and initial test

--- tests/testthat/test-ntwk_info_ig.R
+++ tests/testthat/test-ntwk_info_ig.R
@@ -13,3 +13,14 @@ test_that("ntwk_info_ig returns 16 named vertices for flo_ig", {
   flo_ig_info <- ntwk_info_ig(flo_ig)
   expect_equal(length(flo_ig_info$ni$name), 16)
 })
+
+# Load the statnet version
+data("florentine", package = "ergm")
+flo_sna <- flomarriage
+
+flo_sna <- connect_isolates_sna(flo_sna)
+
+test_that("ntwk_info_stat returns 16 named vertices for flo_sna_info", {
+  flo_sna_info <- ntwk_info_sna(flo_sna)
+  expect_equal(length(flo_sna_info$ni$name), 16)
+})
