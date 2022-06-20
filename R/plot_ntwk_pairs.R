2efc085662870b0ae7b24b92e7d19f35545d4c14
Steve Linberg <steve@slinberg.net>
2022-06-20 12:18:23 -0400

    add docs, tweak plot

--- R/plot_ntwk_pairs.R
+++ R/plot_ntwk_pairs.R
@@ -1,3 +1,13 @@
+#' Title
+#'
+#' @param ig_nodes a list of igraph nodes from `ntwk_info_ig`
+#' @param sna_nodes a list of statnet nodes from `ntwk_info_sna`
+#' @param col_name the name of the column to create data pairs for
+#'
+#' @return a list of paired vectors of values, suitable to pass to `plot_ntwk_vector_pairs`
+#' @export
+#'
+#' @examples
 ntwk_vector_pairs <- function(ig_nodes, sna_nodes, col_name) {
   temp_df <- data.frame(
     name    = ig_nodes$name,
@@ -9,12 +19,21 @@ ntwk_vector_pairs <- function(ig_nodes, sna_nodes, col_name) {
   return(output_list)
 }
 
+#' Title
+#'
+#' @param ntwk_list a list of vector pairs from `newk_vector_pairs`
+#' @importFrom dplyr %>%
+#'
+#' @return
+#' @export
+#'
+#' @examples
 plot_ntwk_vector_pairs <- function(ntwk_list) {
   df_tall <- ntwk_list$df %>%
     tidyr::pivot_longer(!name, names_to = "package", values_to = ntwk_list$col_name)
 
-  ggplot(data = df_tall, aes(name, .data[[ntwk_list$col_name]], fill = package)) +
-    geom_col(position = "dodge", color = "black") +
-    coord_flip() +
-    theme_minimal()
+  ggplot2::ggplot(data = df_tall, ggplot2::aes(name, .data[[ntwk_list$col_name]], fill = package)) +
+    ggplot2::geom_col(position = "dodge", color = "black") +
+    ggplot2::coord_flip() +
+    ggplot2::theme_minimal()
 }
